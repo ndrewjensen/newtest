813016f801ef2a5718fd93da9b7be50e40e3b8e9
Andrew Jensen <andrewkjensen@gmail.com>
2022-10-26 09:56:15 -0700

    update favicon and remove unused import

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import userContext from "../userContext";
 
